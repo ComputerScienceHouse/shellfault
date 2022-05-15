870adab9f7d4778297b253f849e6d9c496b6b123
Mary Strodl <ipadlover8322@gmail.com>
2022-05-15 09:19:57 -0400

    ingest: working ingest script

---
+++ README.md
@@ -0,0 +1,9 @@
+# Shellfault
+
+Quotefault ingest script to convert quotes into fortune files.
+
+
+Dependencies:
+* `curl` to grab quotes
+* `jq`, to convert quotes.
+* Requires a service account at `$SHELLFAULT_SERVICE_TOKEN`
---
+++ ingest.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+_ACCESS_TOKEN=$(
+  curl https://sso.csh.rit.edu/auth/realms/csh/protocol/openid-connect/token \
+    --basic -u shellfault:$SHELLFAULT_SERVICE_TOKEN \
+    -d 'grant_type=client_credentials' \
+    -d 'audience=quotefault' | jq -r .access_token
+)
+
+_GET_QUOTES="curl https://quotefault.csh.rit.edu/storage -H 'Accept: application/json' -H 'Authorization: Bearer $_ACCESS_TOKEN'"
+
+sh -c "$_GET_QUOTES" | \
+  TZ=America/New_York jq -j '[.quotes | .[] | (
+    .quote +
+    "\n\t\t-- " + .speaker +
+    ", (Submitted by: " + .submitter + "), " +
+    (.time | sub("\\+00:00$"; "Z") | fromdateiso8601 | strflocaltime("%F %T")) + "\n%"
+  )] | sort | join("\n")' > /etc/fortune/csh
+
+strfile /etc/fortune/csh
